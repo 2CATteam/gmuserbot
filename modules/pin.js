5ef17bb3b3393fb81c75c3af8096bc11dd153ab2
Daniel Royer <2CATteam@gmail.com>
2020-02-28 16:09:39 +0000

    Added @all

--- modules/pin.js
+++ modules/pin.js
@@ -11,6 +11,7 @@ exports.mod = class pin {
 	checkMessage(message, token) {
 		if (!message.text) {return false}
 		var text = message.text.match(/^\/pin\s(.+)/i)
+		var num = message.text.match(/^\/unpin\s?(\d+)\s*$/i)
 		if (message.text.match(/^\/pins/i)) {
 			var toSend = "Here are all the pinned messages in this chat:\n\n"
 			var id = pins[message.group_id] ? message.group_id : message.chat_id
@@ -18,16 +19,23 @@ exports.mod = class pin {
 				toSend += "Nothing has been pinned yet!\n"
 			} else {
 				for (var i in pins[id]) {
-					toSend += pins[id][i]
-					toSend += "\n"
+					toSend += `${parseInt(i)+1}: ${pins[id][i]}\n`
 				}
 			}
-			toSend += "\nTo add messages, use /pin [something]."
+			toSend += "\nTo add messages, use /pin [something]. To remove a message, use /unpin [number]"
 			sender.send(toSend, token, message)
 		} else if (text && (message.sender_id != require('../res.json').user_id)) {
 			sender.like(message, token)
 			this.pin(message, message.name + ": " + text[1])
 			return true
+		} else if (num) {
+			sender.like(message, token)
+			var id = message.group_id
+			if (!id) { id = message.chat_id }
+			if (pins[id]) { if (parseInt(num[1])-1 < pins[id].length) {
+				pins[id].splice(parseInt(num[1])-1, 1)
+			}}
+			fs.writeFileSync(__dirname + '/res/pins.json', JSON.stringify(pins, null, "\t"), 'utf8')
 		}
 		return false
 	}
